dac6f29917f919961f71b1ab886d0821b66731c4
Pablo Pastor Martín <alu0100890839@ull.edu.es>
2018-06-01 14:46:14 +0100

    Dense

---
+++ view_fc.py
@@ -0,0 +1,28 @@
+from keras.applications import VGG16
+from vis.utils import utils
+from keras import activations
+from vis.visualization import visualize_activation
+from matplotlib import pyplot as plt
+from vis.input_modifiers import Jitter
+import numpy as np
+import os
+
+categorias = np.random.permutation(1000)[:15]
+
+model = VGG16(weights='imagenet', include_top=True)
+layer_idx = utils.find_layer_idx(model, 'predictions') # -1 tambien vale (es la ultima)
+
+#Softmax a linear
+
+model.layers[layer_idx].activation = activations.linear
+model = utils.apply_modifications(model)
+
+if not os.path.isdir("fc"):
+    os.mkdir("fc")
+
+for categoria in categorias:
+    img = visualize_activation(model, layer_idx, filter_indices=categoria,max_iter=500, input_modifiers=[Jitter(16)])
+    img = utils.draw_text(img, utils.get_imagenet_label(categoria))
+    plt.imshow(img)
+    plt.savefig("fc/" + utils.get_imagenet_label(categoria)+ ".png", dpi=3000)
+    #plt.show()
